475c64deecc0b421625c57286e5958b65a93bff0
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2024-01-15 13:56:03 +0300

    Adding local custom font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import localFont from 'next/font/local'
+
+import './globals.css'
+const GraphikLC = localFont({ src: './graphiklcweb_medium.ttf' })
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={GraphikLC.className}>{children}</body>
     </html>
   )
 }
